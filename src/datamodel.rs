c107854229ca3f8bb1092753d4613b5556749787
Aristarkh Zagorodnikov <xm@drive.net>
2021-06-15 21:52:56 +0300

    Cleanup

--- src/datamodel.rs
+++ src/datamodel.rs
@@ -52,7 +52,7 @@ impl TryFrom<&str> for Record {
         const FILTER_REGEX_PATTERN: &str = r"^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d+)?Z) (\S+?):(\d{1,5}) (\S+?):(\d{1,5}) (\S*) ([0-9a-fA-F]{1,4}) ([0-9a-fA-F]{64}) ([0-9a-fA-F]{64}) ([0-9a-fA-F]{16,})$";
         lazy_static! {
             static ref FILTER_REGEX: Regex =
-                Regex::new(FILTER_REGEX_PATTERN).expect("Failed to parse filter regex.");
+                Regex::new(FILTER_REGEX_PATTERN).expect("Failed to parse filter regex");
         }
 
         let captures = FILTER_REGEX
